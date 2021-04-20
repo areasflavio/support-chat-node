f72fb276d3b42ae9189b8a78f1b5d99b0ccd9343
Flávio Arêas <vflavio9@gmail.com>
2021-04-20 16:29:43 -0300

    feat: add routes

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,11 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 
 import './database';
+import { routes } from './routes';
 
 const server = express();
 
 server.use(express.json());
-
-server.get('/', (request: Request, response: Response) => {
-  return response.json({ message: 'Hello world' });
-});
-
-server.post('/users', (request: Request, response: Response) => {
-  const { name, email } = request.body;
-
-  return response.json({ name, email });
-});
+server.use(routes);
 
 server.listen(3333, () => console.log('Server is up on port 3333...'));
