5d24043be866cb13d273b09c79c858998d6671da
Flávio Arêas <vflavio9@gmail.com>
2021-04-21 16:50:14 -0300

    feat: messages entity

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { MessagesController } from './controllers/MessagesController';
 
 import { SettingsController } from './controllers/SettingsController';
 import { UserController } from './controllers/UserController';
@@ -7,8 +8,13 @@ const routes = Router();
 
 const settingsController = new SettingsController();
 const usersController = new UserController();
+const messagesController = new MessagesController();
 
 routes.post('/settings', settingsController.create);
+
 routes.post('/users', usersController.create);
+
+routes.post('/messages', messagesController.create);
+routes.get('/messages/:id', messagesController.showByUser);
 
 export { routes };
