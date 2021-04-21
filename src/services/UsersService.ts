0aa5612f5651b4d45ac5ce810e37945f38dd0f13
Flávio Arêas <vflavio9@gmail.com>
2021-04-21 15:49:27 -0300

    feat: user entity

---
+++ src/services/UsersService.ts
@@ -0,0 +1,22 @@
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/UserRepository';
+
+class UsersService {
+  async create(email: string) {
+    const usersRepository = getCustomRepository(UserRepository);
+
+    const userExists = await usersRepository.findOne({ email });
+
+    if (userExists) {
+      return userExists;
+    }
+
+    const user = usersRepository.create({ email });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { UsersService };
