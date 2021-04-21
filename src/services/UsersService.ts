a37aa61d13695217a7a150822525784ee0b02f40
Flávio Arêas <vflavio9@gmail.com>
2021-04-21 16:53:11 -0300

    refactor: encapsulate common variables in methods

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,19 +1,24 @@
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, Repository } from 'typeorm';
+import { User } from '../entities/User';
 import { UserRepository } from '../repositories/UserRepository';
 
 class UsersService {
-  async create(email: string) {
-    const usersRepository = getCustomRepository(UserRepository);
+  private usersRepository: Repository<User>;
 
-    const userExists = await usersRepository.findOne({ email });
+  constructor() {
+    this.usersRepository = getCustomRepository(UserRepository);
+  }
+
+  async create(email: string) {
+    const userExists = await this.usersRepository.findOne({ email });
 
     if (userExists) {
       return userExists;
     }
 
-    const user = usersRepository.create({ email });
+    const user = this.usersRepository.create({ email });
 
-    await usersRepository.save(user);
+    await this.usersRepository.save(user);
 
     return user;
   }
